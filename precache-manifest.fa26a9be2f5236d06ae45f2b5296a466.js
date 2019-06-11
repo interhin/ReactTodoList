770760e49f02f1eff6e10db7842ec3917600ce9a
Dmitriy Vergulev <diman7277@gmail.com>
2019-06-11 21:40:31 +0700

    Updates

---
+++ precache-manifest.fa26a9be2f5236d06ae45f2b5296a466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee47cf4a22c7196c0d5f9ec0047e3e9",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "707d6b4773af09699126",
+    "url": "/ReactTodoList/static/css/main.28a56cb3.chunk.css"
+  },
+  {
+    "revision": "53b0cbec0584efb29fa3",
+    "url": "/ReactTodoList/static/js/2.ef5cab02.chunk.js"
+  },
+  {
+    "revision": "707d6b4773af09699126",
+    "url": "/ReactTodoList/static/js/main.ac40e860.chunk.js"
+  },
+  {
+    "revision": "acf3c240a91029c264ac",
+    "url": "/ReactTodoList/static/js/runtime~main.d5202348.js"
+  }
+]);
